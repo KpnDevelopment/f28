c7e0126349a7dfe679d9c29f2418dcd347609fcd
kpndevelopment <vishnukpn884@gmail.com>
2020-04-18 14:32:14 +0530

    version 1.0

--- app/src/main/java/com/example/androidnav/Adapter/Acdmynotification.java
+++ app/src/main/java/com/example/androidnav/Adapter/Acdmynotification.java
@@ -1,4 +1,46 @@
 package com.example.androidnav.Adapter;
 
-public class  Acdmynotification {
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.androidnav.R;
+
+public class  Acdmynotification extends RecyclerView.Adapter<Acdmynotification.viewholder>{
+    private LayoutInflater inflater;
+    private Context context;
+
+    public Acdmynotification(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        inflater=LayoutInflater.from(context);
+        View view=inflater.inflate(R.layout.fragment_academy,parent,false);
+        viewholder holder=new viewholder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder {
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
