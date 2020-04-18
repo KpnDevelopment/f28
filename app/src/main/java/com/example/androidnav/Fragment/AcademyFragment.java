9080d14d01883c24881d39d33487ba9552108ae4
kpndevelopment <vishnukpn884@gmail.com>
2020-04-18 15:17:33 +0530

    version 1.0

--- app/src/main/java/com/example/androidnav/Fragment/AcademyFragment.java
+++ app/src/main/java/com/example/androidnav/Fragment/AcademyFragment.java
@@ -3,17 +3,24 @@ package com.example.androidnav.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.example.androidnav.Adapter.Acdmynotification;
 import com.example.androidnav.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AcademyFragment extends Fragment {
+    private RecyclerView acdamynotify;
+    private LinearLayout acadmylayout;
+    private Acdmynotification adapter_ac;
 
     public AcademyFragment() {
         // Required empty public constructor
@@ -23,7 +30,17 @@ public class AcademyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_academy, container, false);
+
+//     Inflate the layout for this fragment;
+        View view= inflater.inflate(R.layout.fragment_academy, container, false);
+        acdamynotify=view.findViewById(R.id.academyrecycler);
+        acadmylayout=view.findViewById(R.id.acdamylenear);
+        LinearLayoutManager Lmgr = new LinearLayoutManager(getContext());
+        RecyclerView.LayoutManager recylmgr=Lmgr;
+        acdamynotify.setLayoutManager(recylmgr);
+        adapter_ac=new Acdmynotification(getContext());
+        acdamynotify.setAdapter(adapter_ac);
+        return view;
+
     }
 }
