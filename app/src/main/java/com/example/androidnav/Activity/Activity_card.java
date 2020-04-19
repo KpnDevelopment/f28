9559066aaa917edadedf06c803beee5296ea2b39
kpndevelopment <vishnukpn884@gmail.com>
2020-04-19 22:48:56 +0530

    version 1.0

---
+++ app/src/main/java/com/example/androidnav/Activity/Activity_card.java
@@ -0,0 +1,16 @@
+package com.example.androidnav.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidnav.R;
+
+public class Activity_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+    }
+}
