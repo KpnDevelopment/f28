4102cb582003b1639abe51212e71c2dc2de2e5d3
Adarsh <lonewolffx101@gmail.com>
2020-04-27 14:51:01 +0530

    card view update 1

---
+++ app/src/main/java/com/example/androidnav/Activity/manage_activity.java
@@ -0,0 +1,16 @@
+package com.example.androidnav.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.androidnav.R;
+
+public class manage_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_activity);
+    }
+}
